8c378055850a2c88eaeeb245003a72c3c5f53b29
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2020-09-02 13:33:16 -0400

    Restore original class files

--- module_3/sequence_test.cpp
+++ module_3/sequence_test.cpp
@@ -1,110 +1,80 @@
-/**
- * @file    sequence_test.cpp
- * @brief   An interactive test program for the new sequence class
- * 
- * Note: I have modified this program slightly, to add checks which ensure that
- * the preconditions outlined in the `sequence.h` documentation are met.
- */
-#include "sequence1.h" // With value_type defined as double
-#include <cctype>      // Provides toupper
-#include <cstdlib>     // Provides EXIT_SUCCESS
-#include <iostream>    // Provides cout and cin
+// FILE: sequence_test.cxx
+// An interactive test program for the new sequence class
+#include <cctype>       // Provides toupper
+#include <iostream>     // Provides cout and cin
+#include <cstdlib>      // Provides EXIT_SUCCESS
+#include "sequence1.h"  // With value_type defined as double
 using namespace std;
 using namespace main_savitch_3;
 
 // PROTOTYPES for functions used by this test program:
+void print_menu( );
+// Postcondition: A menu of choices for this program has been written to cout.
 
-/**
- * Postcondition: A menu of choices for this program has been written to cout.
- */
-void print_menu();
-/**
- * Postcondition: The user has been prompted to enter a one character command.
- * The next character has been read (skipping blanks and newline characters),
- * and this character has been returned.
- */
-char get_user_command();
-/**
- * Postcondition: The items on display have been printed to cout (one per line).
- */
+char get_user_command( );
+// Postcondition: The user has been prompted to enter a one character command.
+// The next character has been read (skipping blanks and newline characters), 
+// and this character has been returned.
+
 void show_sequence(sequence display);
-/** 
- * Postcondition: The user has been prompted to enter a real number. The
- * number has been read, echoed to the screen, and returned by the function.
- */
-double get_number();
-/** Main method; executes test program. */
-int main() {
-    sequence test; // A sequence that we'll perform tests on
-    char choice;   // A command character entered by the user
+// Postcondition: The items on display have been printed to cout (one per line).
 
+double get_number( );
+// Postcondition: The user has been prompted to enter a real number. The
+// number has been read, echoed to the screen, and returned by the function.
+
+
+int main( )
+{
+    sequence test; // A sequence that well perform tests on
+    char choice;   // A command character entered by the user
+    
     cout << "I have initialized an empty sequence of real numbers." << endl;
-    do {
-        print_menu();
-        choice = toupper(get_user_command());
-        switch (choice) {
-        case '!':
-            test.start();
-            break;
-        case '+':
-            if (test.is_item())
-                test.advance();
-            else
-                cout << "There is no current item. Could not advance." << endl;
-            break;
-        case '?':
-            if (test.is_item())
-                cout << "There is an item." << endl;
-            else
-                cout << "There is no current item." << endl;
-            break;
-        case 'C':
-            if (test.is_item())
-                cout << "Current item is: " << test.current() << endl;
-            else
-                cout << "There is no current item." << endl;
-            break;
-        case 'P':
-            show_sequence(test);
-            break;
-        case 'S':
-            cout << "Size is " << test.size() << '.' << endl;
-            break;
-        case 'I':
-            if (test.size() < test.CAPACITY)
-                test.insert(get_number());
-            else
-                cout << "Sequence is full. Could not insert." << endl;
-            break;
-        case 'A':
-            if (test.size() < test.CAPACITY)
-                test.attach(get_number());
-            else
-                cout << "Sequence is full. Could not attach." << endl;
-            break;
-        case 'R':
-            if (test.is_item()) {
-                test.remove_current();
-                cout << "The current item has been removed." << endl;
-            } else {
-                cout << "There is no current item. Could not remove." << endl;
-            }
-            break;
-        case 'Q':
-            cout << "Ridicule is the best test of truth." << endl;
-            break;
-        default:
-            cout << choice << " is invalid." << endl;
+
+    do
+    {
+        print_menu( );
+        choice = toupper(get_user_command( ));
+        switch (choice)
+        {
+            case '!': test.start( );
+                      break;
+            case '+': test.advance( );
+                      break;
+            case '?': if (test.is_item( ))
+                          cout << "There is an item." << endl;
+                      else 
+                          cout << "There is no current item." << endl;
+                      break;
+            case 'C': if (test.is_item( ))
+                           cout << "Current item is: " << test.current( ) << endl;
+                      else
+                          cout << "There is no current item." << endl;
+                      break;
+            case 'P': show_sequence(test);
+                      break;
+            case 'S': cout << "Size is " << test.size( ) << '.' << endl;
+                      break;
+            case 'I': test.insert(get_number( ));
+                      break;
+            case 'A': test.attach(get_number( ));
+                      break;
+            case 'R': test.remove_current( );
+                      cout << "The current item has been removed." << endl;
+                      break;     
+            case 'Q': cout << "Ridicule is the best test of truth." << endl;
+                      break;
+            default:  cout << choice << " is invalid." << endl;
         }
-    } while ((choice != 'Q'));
+    }
+    while ((choice != 'Q'));
 
     return EXIT_SUCCESS;
 }
 
-// IMPLEMENTATIONS
-
-void print_menu() {
-    // Library facilities used: iostream.h
+void print_menu( )
+// Library facilities used: iostream.h
+{
     cout << endl; // Print blank line before the menu
     cout << "The following choices are available: " << endl;
     cout << " !   Activate the start( ) function" << endl;
@@ -117,11 +87,11 @@ void print_menu() {
     cout << " A   Attach a new number with the attach(...) function" << endl;
     cout << " R   Activate the remove_current( ) function" << endl;
     cout << " Q   Quit this test program" << endl;
-    cout << endl; // Print blank line after the menu
 }
 
-char get_user_command() {
-    // Library facilities used: iostream
+char get_user_command( )
+// Library facilities used: iostream
+{
     char command;
 
     cout << "Enter choice: ";
@@ -130,19 +100,20 @@ char get_user_command() {
     return command;
 }
 
-void show_sequence(sequence display) {
-    // Library facilities used: iostream
-    for (display.start(); display.is_item(); display.advance()) {
-        cout << display.current() << endl;
-    }
+void show_sequence(sequence display)
+// Library facilities used: iostream
+{
+    for (display.start( ); display.is_item( ); display.advance( ))
+        cout << display.current( ) << endl;
 }
 
-double get_number() {
-    // Library facilities used: iostream
+double get_number( )
+// Library facilities used: iostream
+{
     double result;
-
+    
     cout << "Please enter a real number for the sequence: ";
-    cin >> result;
+    cin  >> result;
     cout << result << " has been read." << endl;
     return result;
 }
