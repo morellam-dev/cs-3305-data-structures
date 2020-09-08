d57d3f77844952b5a2f52a0a6f13269212ed71a0
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2020-09-08 12:52:25 -0400

    Update documentation in header

--- module_4/poly0.h
+++ module_4/poly0.h
@@ -7,8 +7,6 @@
 #pragma once
 
 #include <iostream>  // Provides ostream
-// If your compiler does not support namespaces, then please delete the
-// following line and the set of brackets that follow.
 namespace main_savitch_3 {
 class polynomial {
  public:
@@ -16,84 +14,69 @@ class polynomial {
   static const size_t CAPACITY = 2048;
   // The maximum exponent which can be safely stored.
   static const unsigned int MAX_EX = CAPACITY - 1;
+  // Constructs a new polynomial. If specified, the given exponent will have the
+  // given exponent.
   // PRECONDITION: exponent <= CAPACITY
-  // POSTCONDITION: This polynomial has been create with all zero
-  // coefficients, except for coefficient c for the specified exponent.
-  // When used as a default constructor (using default values for both
-  // arguments), the result is a polynomial with all zero coefficients.
   polynomial(double c = 0.0, unsigned int exponent = 0);
-  // POSTCONDITION: This polynomial has been created as a deep copy of the given
-  // polynomial.
+  // Constructs a deep copy of another polynomial
   polynomial(const polynomial& p2);
 
   // MODIFICATION MEMBER FUNCTIONS
 
+  // Adds the given amount to the coefficient of the specified exponent.
   // PRECONDITION: exponent <= CAPACITY
-  // POSTCONDITION: Adds the given amount to the coefficient of the specified
-  // exponent.
   void add_to_coef(double amount, unsigned int exponent);
+  // Sets the coefficient for the specified exponent.
   // PRECONDITION: exponent <= CAPACITY
-  // POSTCONDITION: Sets the coefficient for the specified exponent.
   void assign_coef(double coefficient, unsigned int exponent);
-  // POSTCONDITION: All coefficients of this polynomial are set to zero.
+  // Sets all coefficients of this polynomial to zero.
   void clear();
   // POSTCONDITION: This polynomial is a deep copy of the given polynomial.
   void operator=(const polynomial& p2);
-  // POSTCONDITION: the polynomial has been destructed and its allocated memory
-  // cleared.
+  // frees up allocated memory when this polynomial is deleted
   ~polynomial();
 
   // CONSTANT MEMBER FUNCTIONS
 
-  // POSTCONDITION: Returns coefficient at specified exponent of this
-  // polynomial.
+  // @returns the coefficient of the term with the specified exponent
   double coefficient(unsigned int exponent) const;
-  // POSTCONDITION: The function returns the value of the largest exponent
-  // with a non-zero coefficient.
-  // If all coefficients are zero, then the function returns zero.
+  // @returns the largest exponent with a non-zero coefficient, otherwise 0
   inline unsigned int degree() const { return current_degree; }
-  // POSTCONDITION: The return value is the value of this polynomial with
-  // the given value for the variable x.
+  // @returns the calculated value of this polynomial with the given value for
+  // the variable x.
   double eval(double x) const;
-  // POSTCONDITION: The return value is the next exponent n which is LARGER
-  // than e such that coefficient(n) != 0.
-  // If there is no such term, then the return value is zero.
+  // @returns the next exponent n which is LARGER than e such that
+  // coefficient(n) != 0. If there is no such term, the return value is zero.
   unsigned int next_term(unsigned int e) const;
-  // POSTCONDITION: The return value is the next exponent n which is SMALLER
-  //  than e such that coefficient(n) != 0.
-  // If there is no such term, then the return value is UINT_MAX from <climits>.
+  // @returns the next exponent n which is SMALLER than e such that
+  // coefficient(n) != 0. If there is no such term, the return value is UINT_MAX
+  // from <climits>.
   unsigned int previous_term(unsigned int e) const;
 
   // CONSTANT OPERATORS
 
-  // POSTCONDITION: The return value is the value of this polynomial with
-  // the given value for the variable x.
-  double operator()(double x) const { return eval(x); }
+  // same as eval(x)
+  // @returns the calculated value of this polynomial with the given value for
+  // the variable x.
+  inline double operator()(double x) const { return eval(x); }
 
  private:
   double* m_coef;               // a pointer to a dynamic array
   size_t m_capacity;            // the current size of the dynamic array
   unsigned int current_degree;  // the current degree
-  // POSTCONDITION: current_degree stores the highest degree
+  // recalculates current_degree
   void compute_degree();
-  // POSTCONDITION: capacity is now greater than or equal to minimum_size.
-  void reallocate(unsigned int minimum_size);
+  // resizes the coefficient array to be at least as large as min_size.
+  void reallocate(unsigned int min_size);
 };
 
-// NON-MEMBER BINARY OPERATORS
+// NON-MEMBER OPERATOR FUNCTIONS
 
-// POSTCONDITION: return-value is a polynomial with each coefficient
-// equal to the difference of the coefficients of p1 & p2 for any given
-// exponent.
+// @returns a polynomial which is the difference between p1 and p2
 polynomial operator+(const polynomial& p1, const polynomial& p2);
-// POSTCONDITION: return-value is a polynomial with each coefficient
-// equal to the sum of the coefficients of p1 & p2 for any given
-// exponent.
+// @returns a polynomial which is the sum of p1 and p2
 polynomial operator-(const polynomial& p1, const polynomial& p2);
-
-// NON-MEMBER OUTPUT FUNCTIONS
-
-// POSTCONDITION: The polynomial has been printed to ostream out, which,
-// in turn, has been returned to the calling function.
+// Prints a textual representation of the polynomial
+// @returns the given stream
 std::ostream& operator<<(std::ostream& out, const polynomial& p);
 }  // namespace main_savitch_3
