ea759e1ae2fe8af1c1d31b405b57980e3bbb8682
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2020-10-05 16:18:55 -0400

    Change deque::operator= to be a void function.

--- module_7/deque.h
+++ module_7/deque.h
@@ -24,7 +24,7 @@ class deque {
   deque(const deque<T>& dq);
 
   // postcondition: current deque is a copy of dq
-  deque<T>& operator=(const deque<T>& dq);
+  void operator=(const deque<T>& dq);
 
   // precondition: deque is not empty
   // postcondition: reference to element at front of deque
@@ -37,46 +37,41 @@ class deque {
   T front() const;
 
   // precondition: deque is not empty
-  // postcondition: reference to element at front of deque
-  //                            has been returned
+  // postcondition: reference to element at front of deque has been returned
   T& back();
 
   // precondition: deque is not empty
-  // postcondition: copy of element at back of deque
-  //                            has been returned
+  // postcondition: copy of element at back of deque has been returned
   T back() const;
 
   // precondition: deque is not full
-  // postcondition: entry has been inserted at the front
-  //                            of the deque
+  // postcondition: entry has been inserted at the front of the deque
   void push_front(const T& entry);
 
-  // precondition: deque is not full
-  // postcondition: entry has been inserted at the back
-  //                            of the deque
+  // Precondition: deque is not full
+  // Postcondition: entry has been inserted at the back of the deque
   void push_back(const T& entry);
 
-  // precondition: deque is not empty
-  // postcondition: element at front of deque has been removed
+  // Precondition: deque is not empty
+  // Postcondition: element at front of deque has been removed
   void pop_front();
 
-  // precondition: deque is not empty
-  // postcondition: element at back of deque has been removed
+  // Precondition: deque is not empty
+  // Postcondition: element at back of deque has been removed
   void pop_back();
 
-  // postcondition: number of elements in deque has been returned
+  // @returns the number of elements in the deque.
   size_type size() const;
 
-  // postcondition: whether deque is empty has been returned
+  // @returns whether deque is empty
   bool empty() const;
 
-  // postcondition: returned whether 2 deques are equal - equal is defined
-  //                            as the deques have the same number of elements &
-  //                            corresponding elements are equal
+  // @returns whether 2 deques are equal. i.e. they contain the same
+  // values in the same order.
   template <class U>
   friend bool operator==(const deque<U>& dq1, const deque<U>& dq2);
 
-  // postcondition: dq has been display from front to rear on out
+  // Postcondition: All elements from the deque have been printed to out.
   template <class U>
   friend std::ostream& operator<<(std::ostream& out, const deque<U>& dq);
 
