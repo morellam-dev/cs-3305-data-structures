bb96cecd3f3b83b18cde35fbd5d69b8bfe9a32b3
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2020-10-12 12:44:43 -0400

    Format main.cpp

--- module_8/main.cpp
+++ module_8/main.cpp
@@ -1,28 +1,23 @@
+// main.cpp
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 // Function 1
+// Postcondition: Prints the prefix augmented by the section number for each
+// section. Levels determines how deep the sections go (i.e. 2 levels
+// gives 1.1., 1.2., and so on; 3 levels gives 1.1.1., 1.1.2., and so on.)
 void numbers(ostream& outs, const string& prefix, unsigned int levels){
-    // Post-Condition: Prints the prefix augmented by the section
-    //                 number for each section. Levels determines how deep the
-    //                 sections go (i.e. 2 levels gives 1.1., 1.2., and so on;
-    //                 3 levels gives 1.1.1., 1.1.2., and so on.)
-
     // WRITE YOUR CODE HERE
-
 };
 
 // Function 2
+// Precondition:   n is a power of 2 greater than zero.
+// Postcondition: A pattern has been printed to the ostream outs. The longest
+// line of the pattern has n stars beginning in column i of the output.
 void pattern(ostream& outs, unsigned int n, unsigned int i){
-    // Pre-Condition: n is a power of 2 greater than zero.
-    // Post-Condition: A pattern has been printed to the ostream outs.
-    //                 The longest line of the pattern has n stars beginning in
-    //                 column i of the output.
-
     // WRITE YOUR CODE HERE
-
 };
 
 int main(int argc, char** argv) {
