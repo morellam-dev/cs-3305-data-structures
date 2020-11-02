814cf07100d872cd95266f3491cad31675de5050
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2020-11-02 16:20:21 -0500

    Create BST main implementation

--- 10-binary-search-tree/main.cpp
+++ 10-binary-search-tree/main.cpp
@@ -3,6 +3,26 @@
 #include "bst.h"
 using namespace std;
 
-int main() {
+/* A function that constructs Balanced Binary Search Tree from a sorted array */
+void insert_inorder(binary_search_tree<string> &bst, string arr[], int start,
+                    int end) {
+  if (start > end) return;
+  int mid = (start + end) / 2;
+  bst.insert(arr[mid]);
+  insert_inorder(bst, arr, start, mid - 1);
+  insert_inorder(bst, arr, mid + 1, end);
+}
 
+int main() {
+  binary_search_tree<string> bst;
+  string data[]{"after", "also",  "any",   "back",  "because", "come", "day",
+                "even",  "first", "give",  "how",   "its",     "look", "most",
+                "new",   "now",   "only",  "other", "our",     "over", "than",
+                "then",  "these", "think", "two",   "us",      "use",  "want",
+                "way",   "well",  "work"};
+  size_t size = 31;
+  insert_inorder(bst, data, 0, size - 1);
+  std::cout << bst << "\n";
+  std::cout << "Depth: " << check_balanced(bst.get_root()) << "\n";
+  return 0;
 }
