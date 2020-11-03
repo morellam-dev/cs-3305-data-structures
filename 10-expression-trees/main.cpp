f159b0b0ae39e9149fd85f0e87fe279c76faa94f
morellam-dev <4561733+morellam-dev@users.noreply.github.com>
2020-11-02 22:35:26 -0500

    Add a second case to ExprTree example program

--- 10-expression-trees/main.cpp
+++ 10-expression-trees/main.cpp
@@ -4,7 +4,13 @@ using namespace exprtree;
 
 int main() {
   // Sample usage of ExpressionTree
-  ExpressionTree e("3 + 7 * 14");
-  std::cout << e << '\n';
-  std::cout << e.evaluate() << '\n'; // -> 140
+  std::string s1 = "3 + 7 * 14";
+  std::cout << "Generating an ExpressionTree from '" << s1 << "'\n";
+  ExpressionTree e1(s1);
+  std::cout << e1 << " = " << e1.evaluate() << '\n';
+
+  std::string s2 = "1 + 2 * 3";
+  std::cout << "Generating an ExpressionTree from '" << s2 << "'\n";
+  ExpressionTree e2(s2);
+  std::cout << e2 << " = " << e2.evaluate() << '\n';
 }
